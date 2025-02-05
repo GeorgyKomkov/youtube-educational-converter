a9ab1b5403f2a64fe751ae4e9badb2a33966cfd9
Georgy Komkoc <komkov.g.o@yandex.com>
2025-02-05 19:35:40 +0300

    fix

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "Запуск приложения на порту $PORT..."
+echo "Текущая директория: $(pwd)"
+echo "Содержимое директории src:"
+ls -la src/
 gunicorn --bind 0.0.0.0:$PORT src.main:app
