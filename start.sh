48afdfb8a04f9c8ad9cd697fb9be81274b193687
Your Name <you@example.com>
2025-02-16 12:24:11 +0300

    Оптимизация Gunicorn, снижение RAM

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 export PYTHONPATH="/app:$PYTHONPATH"
 export MALLOC_ARENA_MAX=2  # Ограничиваем максимальное использование памяти Python
+export OMP_NUM_THREADS=1    # Ограничиваем потоки OpenMP (Whisper)
+export TOKENIZERS_PARALLELISM=false  # Запрещаем многопотоковость для токенизаторов
 PORT=${PORT:-8080}
 echo "Запуск приложения на порту $PORT..."
 
 # Запускаем Flask-приложение через Gunicorn
-cd /app/src && gunicorn --bind 0.0.0.0:$PORT --workers=1 --threads=1 --timeout=300 --max-requests=10 --max-requests-jitter=5 main:app &
+cd /app/src && gunicorn --bind 0.0.0.0:$PORT --workers=1 --threads=1 --timeout=300 --max-requests=5 --max-requests-jitter=2 main:app &
 
 # Запускаем Celery-воркер
 cd /app && celery -A celery worker --loglevel=info --concurrency=1 &
